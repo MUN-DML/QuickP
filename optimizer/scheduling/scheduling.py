ecff764565aa51f82256e7d03e6a318ea8331c84
Hong Wang <hongw@mun.ca>
2024-11-16 01:54:29 -0330

    adjust old simulation code upon the find_non_c_p change

--- optimizer/scheduling/scheduling.py
+++ optimizer/scheduling/scheduling.py
@@ -13,17 +13,16 @@ from optimizer.scheduling.priority_heteroG import priority_queue_max_rank_hetero
 from optimizer.scheduling.priority_min_comp_cost import priority_queue_min_comp_cost
 
 
-def add_topo_order_constraints_with_grouper(model, graph: CompGraph, x, device_ids, finish, start, group_ops_mapping: dict, M, model_type):
+def add_topo_order_constraints_with_grouper(model, graph: CompGraph, x, device_ids, finish, start,
+                                            group_ops_mapping: dict, M, model_type):
     op_group_mapping = graph.create_op_group_id_mapping()
-    non_reachable_pairs = find_non_connected_pairs(graph)
+    non_reachable_pairs, topological_order_mapping = find_non_connected_pairs(graph)
     ungrouped_non_reachable_pairs = []
 
-    for i,j in non_reachable_pairs:
+    for i, j in non_reachable_pairs:
         if i in op_group_mapping and j in op_group_mapping and op_group_mapping[i] == op_group_mapping[j]:
             continue
-        ungrouped_non_reachable_pairs.append((i,j))
-    topological_order = list(nx.topological_sort(graph))
-    topological_order_mapping = {node: index for index, node in enumerate(topological_order)}
+        ungrouped_non_reachable_pairs.append((i, j))
 
     #  scheduling inside each group follows topo sort since each node pair in non_reachable_pairs is calculated by this sort algorithm
     for ops in group_ops_mapping.values():
@@ -32,17 +31,19 @@ def add_topo_order_constraints_with_grouper(model, graph: CompGraph, x, device_i
             model.addConstr(finish[op_a] <= start[op_b])
 
     # Iterate over topologically sorted nodes
-    for a, b in ungrouped_non_reachable_pairs:
+    for device_id in device_ids:
+        for a, b in ungrouped_non_reachable_pairs:
         # For each consecutive pair of operators, add a constraint for each device
-        for device_id in device_ids:
             # Ensure the correct order for each potential device assignment
             # This constraint will only apply if both a and b are assigned to the same device
             model.addConstr(finish[a] <= start[b] + M * (2 - x[a, device_id] - x[b, device_id]),
-                            name="" if model_type in [TFModelEnum.BERT, TFModelEnum.FNET] else f"bigM_topo_order_{a}_{b}_on_device_{device_id}")
+                            name="" if model_type in [TFModelEnum.BERT,
+                                                      TFModelEnum.FNET] else f"bigM_topo_order_{a}_{b}_on_device_{device_id}")
+
 
 def add_topo_order_constraint(model, graph, x, device_ids, finish, start, M):
     # Iterate over topologically sorted nodes
-    non_reachable_pairs = find_non_connected_pairs(graph)
+    non_reachable_pairs, _ = find_non_connected_pairs(graph)
     print('numero de op', len(non_reachable_pairs))
     for a, b in non_reachable_pairs:
         # For each consecutive pair of operators, add a constraint for each device
