e07f4d8dcb3144613d95c29c0d9a75fb6eae1829
hola <hongw@mun.ca>
2024-11-15 22:29:59 -0330

    update visu in icc2025

--- ICC2025/QuickP.py
+++ ICC2025/QuickP.py
@@ -175,7 +175,7 @@ def QuickP(comp_graph: CompGraph, deviceTopo, M, model_type) -> dict:
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='arguments for optimization problem after graph partitioning')
-    parser.add_argument('--number_of_device', type=int, default=6)
+    parser.add_argument('--number_of_device', type=int, default=2)
     # TEST SMALL
     parser.add_argument('--model', type=str, default='ALEXNET')
     parser.add_argument('--normalization_function', default='MIN_MAX', type=str, help='')
@@ -197,6 +197,6 @@ if __name__ == '__main__':
     print("op fusion run time", datetime.timedelta(seconds=ending_time.timestamp() - beginning_time.timestamp()))
     # apply co-location grouper
     apply_all_co_location_constraint(comp_graph, deviceTopo, args.number_of_device)
-    visualize_graph(comp_graph, False, False)
+    comp_graph.visualize_graphviz()
 
     QuickP(comp_graph, deviceTopo, M=get_proper_M(model_type), model_type=model_type)
--- optimizer/operator_device_placement/placement.py
+++ optimizer/operator_device_placement/placement.py
@@ -1,7 +1,6 @@
 from enum import Enum
 
 from optimizer.model.graph import CompGraph, DeviceGraph
-from optimizer.operator_device_placement.non_contiguous_metis.near_optimal_placement import get_near_optimal_placement
 from optimizer.operator_device_placement.metis.metis_partition import metis_partition
 from optimizer.operator_device_placement.metis.subgraph_util import map_subgraph_to_device, construct_sub_graph, \
     identify_edges_cut
@@ -21,7 +20,6 @@ class PlacementGenerator(Enum):
     OPTIMIZED = "OPTIMIZED"
     OPTIMIZED_GROUPER = "OPTIMIZED_GROUPER"
     OPTIMIZED_HOMO = "OPTIMIZED_HOMO"
-    INCONTIGUOUS_METIS = "INCONTIGUOUS_METIS"
     OPTIMIZED_GROUPER_HOMO = "OPTIMIZED_GROUPER_HOMO"
 
 
@@ -45,10 +43,6 @@ def get_placement_info(placement_type: str, comp_graph: CompGraph, device_topo: 
 
     elif placement_type == PlacementGenerator.OPTIMIZED_GROUPER.value:
         operator_device_mapping = get_optimize_placement_with_grouper(comp_graph, device_topo, M, model_type)
-        edge_cut_list, edge_cut_weight_sum = identify_edges_cut(comp_graph, operator_device_mapping)
-
-    elif placement_type == PlacementGenerator.INCONTIGUOUS_METIS.value:
-        operator_device_mapping = get_near_optimal_placement(comp_graph, device_topo)
         edge_cut_list, edge_cut_weight_sum = identify_edges_cut(comp_graph, operator_device_mapping)
 
     elif placement_type == PlacementGenerator.OPTIMIZED_HOMO.value:
